2703d00ae48636badf90e1603ff41c8ab25154c5
Patrick Rocha <p.rocha159@gmail.com>
2025-01-30 17:35:11 -0600

    Fix to prevent multiple emails being used. Added error message to page

--- src/main/java/com/example/sampleapp/controller/UsersController.java
+++ src/main/java/com/example/sampleapp/controller/UsersController.java
@@ -2,22 +2,27 @@ package com.example.sampleapp.controller;
 
 import com.example.sampleapp.entity.Users;
 import com.example.sampleapp.entity.UsersType;
+import com.example.sampleapp.services.UsersService;
 import com.example.sampleapp.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UsersController {
 
     private final UsersTypeService usersTypeService;
+    private final UsersService usersService;
 
     @Autowired
-    public UsersController(UsersTypeService usersTypeService) {
+    public UsersController(UsersTypeService usersTypeService, UsersService usersService) {
         this.usersTypeService = usersTypeService;
+        this.usersService = usersService;
     }
 
     @GetMapping("/register")
@@ -26,5 +31,22 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersType);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users, Model model) {
+
+        Optional<Users> optionalUsers = usersService.getUserByEmail(users.getEmail());
+
+        if(optionalUsers.isPresent()) {
+            model.addAttribute("error", "Email already registered, try to login or register with other email.");
+            List<UsersType> usersType = usersTypeService.getAll();
+            model.addAttribute("getAllTypes", usersType);
+            model.addAttribute("user", new Users());
+            return "register";
+        }
+
+        usersService.addNew(users);
+        return "dashboard";
     }
 }
